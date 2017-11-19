b7203e4c38f32066f82ffeb2215ba26cd0eaaed0
Nazmus Saadat <nuhin13@Nazmuss-Mac.local>
2017-11-19 15:24:54 +0600

    2nd

---
+++ IosTableView/View/CategoryCell.swift
@@ -0,0 +1,24 @@
+//
+//  CategoryCell.swift
+//  IosTableView
+//
+//  Created by Nazmus Saadat on 19/11/17.
+//  Copyright © 2017 Nazmus Saadat. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
